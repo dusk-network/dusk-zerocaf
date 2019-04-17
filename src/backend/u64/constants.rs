760d8ebdf12d789a06135d96e4e9212f37d50593
CPereez <carlos@dusk.network>
2019-04-17 17:43:16 +0200

    Add constants for montgomery_reduction algorithm
    - `LFACTOR = -1 (mod 2^52)`
    - `RR = (2^260)^2`

--- src/backend/u64/constants.rs
+++ src/backend/u64/constants.rs
@@ -5,3 +5,9 @@ use crate::backend::u64::scalar::Scalar;
 
 /// `R` is the order of base point, i.e. 2^249 - 15145038707218910765482344729778085401
 pub(crate) const R: Scalar = Scalar([2766226127823335, 4237835465749098, 4503599626623787, 4503599627370495, 2199023255551]);
+
+/// `L` * `LFACTOR` = -1 (mod 2^52)
+pub(crate) const LFACTOR: u64 = 0x51da312547e1b;
+
+/// `R^2` = (2^260)^2
+pub(crate) const RR: Scalar = Scalar([489789243728298, 2151629764949650, 2507032305430425, 456090738750059, 532206071303]);
