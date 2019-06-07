83ef4ee4197a1008506c8f07422abd1dcf837eb3
kr0 <c.perezbaro@gmail.com>
2019-06-07 14:22:06 +0200

    Refactor comments.

--- src/traits.rs
+++ src/traits.rs
@@ -1,13 +1,8 @@
-//! Module for public Trait implementations.
-
-
-
-// Public Traits
+//! Module for Public Trait implementations.
 
 
 /// Trait for getting the identity element of a point type.
 pub trait Identity {
     /// Returns the identity element of the curve.
-    /// Can be used as a constructor.
     fn identity() -> Self;
 }
