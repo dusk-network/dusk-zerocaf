5035a05f3bc5b2c67ecffea5da32d4d6ceeb8671
kr0 <c.perezbaro@gmail.com>
2019-07-03 13:39:53 +0200

    Add badages to the docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+//! <a href="https://codecov.io/gh/dusk-network/Dusk-Zerocaf">
+//! <img src="https://codecov.io/gh/dusk-network/Dusk-Zerocaf/branch/master/graph/badge.svg" />
+//! </a>
+//! <a href="https://travis-ci.com/dusk-network/dusk-zerocaf">
+//! <img src="https://travis-ci.com/dusk-network/dusk-zerocaf.svg?branch=master" />
+//! </a>
+//! </hr>
+//! 
 //! <div>
 //! <img src="https://camo.githubusercontent.com/db129d98b9686d0db27a9fd27c8e54086b14a6a7/68747470733a2f2f692e696d6775722e636f6d2f496a61645a50592e6a7067" width="100%" />
 //! </div>
 //! <hr/>
+
 
 // Used for traits related to constant-time code.
 extern crate subtle;
