fbf52c4723eec405787ff11f7acc3b1c541a38ec
kr0 <c.perezbaro@gmail.com>
2019-07-06 01:30:04 +0200

    Improve `Identity` trait def and create `Square` trait.
    Created `Square` trait since it's an operation
    that has to be performed a lot of times for
    different elements of the lib such as:
    - FieldElement
    - Scalar

--- src/traits.rs
+++ src/traits.rs
@@ -1,8 +1,28 @@
 //! Module for Public Trait implementations.
 
 
-/// Trait for getting the identity element of a point type.
+/// Gives the Identity element for the
+/// type which it has been implemented on.
+/// 
+/// This trait is implemented following the rules that
+/// mandate over the Type that is being implemented. 
 pub trait Identity {
-    /// Returns the identity element of the curve.
+
+    #[must_use]
+    /// Returns the identity element for the implemented
+    /// type, following it's mathematical rules.
     fn identity() -> Self;
+}
+
+/// Trait that represents the `^2` operation for any
+/// kind of element on the library. 
+/// 
+/// This trait is implemented following the rules that
+/// mandate over the Type that is being implemented. 
+pub trait Square {
+    type Output;
+
+    #[must_use]
+    /// Returns the square of the input: `x^2`.
+    fn square(self) -> Self::Output;
 }
