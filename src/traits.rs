fe6e5b440ab8e8dc803465a4a08b6d03066c0e00
kr0 <c.perezbaro@gmail.com>
2019-07-07 16:16:58 +0200

    Define `Half` trait on `traits.rs`.
    Since various types implement it, it's better
    to implement half operation as a trait and impl
    it for the types that need it.

--- src/traits.rs
+++ src/traits.rs
@@ -28,13 +28,35 @@ pub mod ops {
         fn square(self) -> Self::Output;
     }
 
+    /// Trait that represents the Point doubling operation
+    /// for any type of Point that is used on the lib. 
+    /// 
+    /// This trait is implemented following the rules that
+    /// mandate over the Type that is being implemented.
     pub trait Double {
-    type Output;
+        type Output;
 
-    #[must_use]
-    /// Performs the point-doubling operation over the
-    /// coordinates which this trait has been implemented
-    /// for.
-    fn double(self) -> Self::Output;
-}
+        #[must_use]
+        /// Performs the point-doubling operation over the
+        /// coordinates which this trait has been implemented
+        /// for.
+        fn double(self) -> Self::Output;
+    }
+
+    /// Trait that represents the `/2` operation for any
+    /// kind of element on the library.
+    /// 
+    /// This is a more performant way of performing the
+    /// division by 2 that dividing by 2 with the `Div`
+    /// trait implementation.
+    /// 
+    /// This trait is implemented following the rules that
+    /// mandate over the Type that is being implemented.
+    pub trait Half {
+        type Output;
+
+        #[must_use]
+        /// Returns the square of the input: `x/2`.
+        fn half(&self) -> Self::Output;
+    }
 }
