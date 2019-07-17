4fc7eebc222799d72972e50b91f7fb5bfcd2aa66
Carlos Perez <37264926+CPerezz@users.noreply.github.com>
2019-07-17 13:19:48 +0200

    Revert "Merge request of added works "

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,18 +1,17 @@
-
-//! This contains the different backend implementations: `u64` and further comming ones. .
+//! Contains the different backend implementations: `u64` and further comming ones.
 //! 
 //! On this module you can find the different implementations
 //! done for Finite Fields mathematical-backends.
 
 /// The u64 backend contains the implementation of all of the 
 /// mathematical base eg. Arithmetics over Finite Fields with 
-/// a design specially thought out 64-bit architectures.
+/// a design specially thought for 64-bit architectures.
 pub mod u64;
 #[cfg(not(any(
     feature = "u64_backend"
 )))]
 
-// A backend feature must fair be chosen.
+// A backend feature has to be choosen.
 compile_error!(
     "no zerocaf backend cargo feature enabled! \
      please enable u64_backend"
