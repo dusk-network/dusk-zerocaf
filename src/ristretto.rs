29b1ba73099b24fb7a11d6df6671c5d0238a8388
kr0 <c.perezbaro@gmail.com>
2019-08-06 01:41:05 +0200

    Implement `decompression()` for `CompressedRistretto`
    The function attempts to decompress a
    `CompressedRistretto` point and returns None if
    the process fails.

--- src/ristretto.rs
+++ src/ristretto.rs
@@ -16,6 +16,7 @@ use subtle::{Choice, ConditionallyNegatable, ConditionallySelectable, ConstantTi
 /// Ristretto Point expressed in wire format.
 /// Since the Ristretto bytes encoding is canonical,
 /// two points are equal if their encodin form is equal. 
+#[derive(Clone, Copy)]
 pub struct CompressedRistretto(pub [u8; 32]);
 
 impl Index<usize> for CompressedRistretto {
@@ -32,8 +33,70 @@ impl ConstantTimeEq for CompressedRistretto {
 }
 
 impl CompressedRistretto {
-    fn as_bytes(&self) -> [u8; 32] {
+    /// Get the bytes of the `CompressedRistretto` point.
+    pub fn as_bytes(&self) -> [u8; 32] {
         self.0
+    }
+
+    #[allow(non_snake_case)]
+    pub fn decompress(&self) -> Option<RistrettoPoint> {
+        // Step 1: Check that the byte-string is a valid FieldElement. 
+
+        // As Ristretto paper says: "If the implementation's field element
+        // encoding function produces canonical outputs, one way to check 
+        // that s_bytes is a canonical encoding (in step 1) is to decode 
+        // s_bytes into sss, then re-encode sss into s_bytes_check, and ensure 
+        // that s_bytes == s_bytes_check.
+        let s: FieldElement = FieldElement::from_bytes(&self.as_bytes());
+        let s_check = s.to_bytes();
+        let s_correct_enc = s_check.ct_eq(&self.as_bytes());
+        let s_is_positive = s.is_positive();
+
+        // If the byte-encoding was incorrect or the representation is
+        // a negative `FieldElement` (according to the definition of 
+        // positive found on Decaf paper), return `None`. 
+        if s_is_positive.unwrap_u8() == 0u8 || s_correct_enc.unwrap_u8() == 0u8 {
+            return None
+        };
+
+        // Step 2: Attempt to decompress the CompressedRistretto. 
+        let one = FieldElement::one();
+
+        // u1 = 1 + as² with a = -1. 
+        let u1 = one - s.square();
+        // u2 = 1 - as² with a = -1. +
+        let u2 = one + s.square();
+        let u2_sq = u2.square();
+
+        // v = a*d*u1² - u2²
+        let v = -(constants::EDWARDS_D * u1.square()) - u2_sq;
+        // I = 1/sqrt(v*u2²), returns `None` if the sqrt does not exist. 
+        let I = match (v*u2_sq).mod_sqrt(Choice::from(1u8)) {
+            None => return None,
+            Some(x) => x.inverse()
+        };
+        // Compute the Extended Point Coordinates Y & T
+        let Dx = I*u2;
+        let Dy = I*Dx*v;
+
+        // Compute ABS(2*s*Dx) and negate if it is negative.
+        let mut x = (&s + &s) * Dx;
+        let x_is_pos = x.is_positive();
+        x.conditional_negate(!x_is_pos);
+        // Compute Y and T coordinates. 
+        let y = u1 * Dy;
+        let t = x * y;
+
+        if t.is_positive().unwrap_u8() == 0u8 || y == FieldElement::zero() {
+            return None
+        };
+
+        Some(RistrettoPoint(EdwardsPoint {
+            X: x,
+            Y: y,
+            Z: one,
+            T: t
+        }))
     }
 }
 
