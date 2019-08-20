8b0c1cedbe9c12832b1a7c292f255acf3e00bd94
kr0 <c.perezbaro@gmail.com>
2019-08-21 00:36:38 +0200

    Compute Twisted Edwards constants for Ristretto.

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,7 @@
 //! Contains the curve-constants needed by different algorithm implementations.
 
 use crate::field::FieldElement;
+use crate::ristretto::CompressedRistretto;
 
 /// Edwards `a` variable value = `-1 (mod l)` equals:
 /// `7237005577332262213973186563042994240857116359379907606001950938285454250988`
@@ -12,12 +13,18 @@ pub static EDWARDS_A: FieldElement = FieldElement([671914833335276, 391666432510
 /// where `l = Prime of the field = 2^252 + 27742317777372353535851937790883648493`
 pub static EDWARDS_D: FieldElement = FieldElement([939392471225133, 587442007554368, 4497154776428662, 4184267646867733, 2921744366591]);
 
-/// `1/SQRT(a) (mod l)` equals: `3034649101460298094273452163494570791663566989388331537498831373842135895065`.
+/// Ristretto `d = -121665/121666 (mod l) = 3939827596983564740704173820156434201364153519171569545177282234949263247860`.
+pub const RISTRETTO_D: FieldElement = FieldElement([3932549999839732, 251630550069643, 1998572741534892, 2238362972951308, 9577190362565]);
+
+/// `1/SQRT(a) (mod l)` equals: ``.
 pub static INV_SQRT_A: FieldElement = FieldElement([3075585030474777, 2451921961843096, 1194333869305507, 2218299809671669, 7376823328646]);
 
-/// `-SQRT(-1) (mod l)` equals: `3034649101460298094273452163494570791663566989388331537498831373842135895065`. 
+/// `-SQRT(-1) (mod l)` equals: ``. 
 pub static MINUS_SQRT_A: FieldElement = FieldElement([3075585030474777, 2451921961843096, 1194333869305507, 2218299809671669, 7376823328646]);
 
-/// `1/SQRT(a-d) (mod l)` equals: `3034649101460298094273452163494570791663566989388331537498831373842135895065`.
-/// NOT WORKING ATM
-pub(crate) static INV_SQRT_A_MINUS_D: FieldElement = FieldElement([3075585030474777, 2451921961843096, 1194333869305507, 2218299809671669, 7376823328646]);
+/// The Ristretto basepoint, in `CompressedRistretto` format.
+pub const RISTRETTO_BASEPOINT_COMPRESSED: CompressedRistretto =
+    CompressedRistretto([0xe2, 0xf2, 0xae, 0x0a, 0x6a, 0xbc, 0x4e, 0x71,
+                         0xa8, 0x84, 0xa9, 0x61, 0xc5, 0x00, 0x51, 0x5f,
+                         0x58, 0xe3, 0x0b, 0x6a, 0xa5, 0x82, 0xdd, 0x8d,
+                         0xb6, 0xa6, 0x59, 0x45, 0xe0, 0x8d, 0x2d, 0x76]);
