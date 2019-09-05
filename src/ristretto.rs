cd2005ae462bde7072a8b31121190bb40387772a
kr0 <c.perezbaro@gmail.com>
2019-09-05 23:44:27 +0200

    Refactor Compression/Decompression and some related functions
    - constants.rs
    Refactored constants picking the correct symbols for
    sqrt results.
    
    - ristretto.rs
    Refactored some issues on the compression/decompression code.
    
    -field.rs
    Refactored the POS_RANGE.
    Refactored sqrt_ratio_i

--- src/ristretto.rs
+++ src/ristretto.rs
@@ -91,7 +91,7 @@ impl CompressedRistretto {
         // If the byte-encoding was incorrect or the representation is
         // a negative `FieldElement` (according to the definition of 
         // positive found on Decaf paper), return `None`. 
-        println!("{:?} {:?}", s_is_positive.unwrap_u8(), s_correct_enc.unwrap_u8());
+        println!("Is positive? {:?} Is correct encoded?{:?}", s_is_positive.unwrap_u8(), s_correct_enc.unwrap_u8());
         if s_is_positive.unwrap_u8() == 0u8 || s_correct_enc.unwrap_u8() == 0u8 {
             return None
         };
@@ -100,7 +100,7 @@ impl CompressedRistretto {
 
         // u1 = 1 + as² with a = -1. 
         let u1 = one - s.square();
-        // u2 = 1 - as² with a = -1. +
+        // u2 = 1 - as² with a = -1. 
         let u2 = one + s.square();
         let u2_sq = u2.square();
 
@@ -108,7 +108,8 @@ impl CompressedRistretto {
         let v = -(constants::RISTRETTO_D * u1.square()) - u2_sq;
         // I = 1/sqrt(v*u2²), returns `None` if the sqrt does not exist. 
         let (ok, I) = (v*u2_sq).inv_sqrt();
-
+        if ok.unwrap_u8() == 0 {return None};
+        
         // Compute the Extended Point Coordinates Y & T
         let Dx = I*u2;
         let Dy = I*Dx*v;
@@ -121,7 +122,8 @@ impl CompressedRistretto {
         let y = u1 * Dy;
         let t = x * y;
 
-        if ok.unwrap_u8() == 0u8 || t.is_positive().unwrap_u8() == 0u8 || y == FieldElement::zero() {
+        println!("Is QR? {:?}  Is positive? {:?} Y is zero?  {:?}",ok.unwrap_u8(), t.is_positive().unwrap_u8(), y);
+        if t.is_positive().unwrap_u8() == 0u8 || y == FieldElement::zero() {
             return None
         };
 
@@ -156,7 +158,7 @@ impl ConstantTimeEq for RistrettoPoint {
 
 impl PartialEq for RistrettoPoint {
     fn eq(&self, other: &RistrettoPoint) -> bool {
-        self.ct_eq(other).unwrap_u8() == 1u8      
+        self.compress().ct_eq(&other.compress()).unwrap_u8() == 1u8      
     }
 }
 
@@ -262,7 +264,7 @@ impl RistrettoPoint {
     pub fn compress(&self) -> CompressedRistretto {
         let u1 = (self.0.Z + self.0.Y) * (self.0.Z - self.0.Y);
         let u2 = self.0.X * self.0.Y;
-        let (_, I) = (u1 * u2).inv_sqrt();
+        let (_, I) = (u1 * u2.square()).inv_sqrt();
         let D1 = u1 * I;
         let D2 = u2 * I;
         let Zinv = D1 * D2 * self.0.T;
@@ -270,8 +272,8 @@ impl RistrettoPoint {
         let D;
         if (self.0.T * Zinv).is_positive().unwrap_u8() == 0u8 {
             xy = 
-                ((self.0.Y * constants::INV_SQRT_A),
-                self.0.X * constants::MINUS_SQRT_A);
+                (constants::SQRT_MINUS_ONE * self.0.Y,
+                constants::SQRT_MINUS_ONE * self.0.X);
             D = D1 * constants::INV_SQRT_A_MINUS_D;
         } else {
             xy = (self.0.X, self.0.Y);
@@ -332,55 +334,6 @@ mod tests {
     use hex;
     use super::*;
 
-    #[test]
-    fn bad_encodings() {
-        // The following are invalid encodings, which should all be rejected.
-        // These are designed to test each of the checks during decoding.
-
-        let bad_encodings = [
-            // These are all bad because they're non-canonical field encodings.
-            "00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-            "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f",
-            "f3ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f",
-            "edffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f",
-            // These are all bad because they're negative field elements.
-            "0100000000000000000000000000000000000000000000000000000000000000",
-            "01ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f",
-            "ed57ffd8c914fb201471d1c3d245ce3c746fcbe63a3679d51b6a516ebebe0e20",
-            "c34c4e1826e5d403b78e246e88aa051c36ccf0aafebffe137d148a2bf9104562",
-            "c940e5a4404157cfb1628b108db051a8d439e1a421394ec4ebccb9ec92a8ac78",
-            "47cfc5497c53dc8e61c91d17fd626ffb1c49e2bca94eed052281b510b1117a24",
-            "f1c6165d33367351b0da8f6e4511010c68174a03b6581212c71c0e1d026c3c72",
-            "87260f7a2f12495118360f02c26a470f450dadf34a413d21042b43b9d93e1309",
-            // These are all bad because they give a nonsquare x^2.
-            "26948d35ca62e643e26a83177332e6b6afeb9d08e4268b650f1f5bbd8d81d371",
-            "4eac077a713c57b4f4397629a4145982c661f48044dd3f96427d40b147d9742f",
-            "de6a7b00deadc788eb6b6c8d20c0ae96c2f2019078fa604fee5b87d6e989ad7b",
-            "bcab477be20861e01e4a0e295284146a510150d9817763caf1a6f4b422d67042",
-            "2a292df7e32cababbd9de088d1d1abec9fc0440f637ed2fba145094dc14bea08",
-            "f4a9e534fc0d216c44b218fa0c42d99635a0127ee2e53c712f70609649fdff22",
-            "8268436f8c4126196cf64b3c7ddbda90746a378625f9813dd9b8457077256731",
-            "2810e5cbc2cc4d4eece54f61c6f69758e289aa7ab440b3cbeaa21995c2f4232b",
-            // These are all bad because they give a negative xy value.
-            "3eb858e78f5a7254d8c9731174a94f76755fd3941c0ac93735c07ba14579630e",
-            "a45fdc55c76448c049a1ab33f17023edfb2be3581e9c7aade8a6125215e04220",
-            "d483fe813c6ba647ebbfd3ec41adca1c6130c2beeee9d9bf065c8d151c5f396e",
-            "8a2e1d30050198c65a54483123960ccc38aef6848e1ec8f5f780e8523769ba32",
-            "32888462f8b486c68ad7dd9610be5192bbeaf3b443951ac1a8118419d9fa097b",
-            "227142501b9d4355ccba290404bde41575b037693cef1f438c47f8fbf35d1165",
-            "5c37cc491da847cfeb9281d407efc41e15144c876e0170b499a96a22ed31e01e",
-            "445425117cb8c90edcbc7c1cc0e74f747f2c1efa5630a967c64f287792a48a4b",
-            // This is s = -1, which causes y = 0.
-            "ecffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f",
-        ];
-
-        let mut bad_bytes = [0u8; 32];
-        for bad_encoding in &bad_encodings {
-            bad_bytes.copy_from_slice(&hex::decode(bad_encoding).unwrap());
-            assert!(CompressedRistretto(bad_bytes).decompress().is_none());
-        }
-    }
-
     #[ignore]
     #[test]
     fn ristretto_point_encoding() {
@@ -423,20 +376,17 @@ mod tests {
 
     #[test]
     fn point_comp_decomp_equivalence() {
-        // This should give the CompressedRistretto Basepoint. 
-        assert!(constants::RISTRETTO_BASEPOINT.compress().decompress().unwrap() == constants::RISTRETTO_BASEPOINT);
+        // This should give the RISTRETTO_BASEPOINT. 
+        let bc = constants::RISTRETTO_BASEPOINT.compress();
+        println!("{:?}", bc);
+        let bcd = bc.decompress().unwrap();
+        println!("{:?}", bcd);
+        let bcdc = bcd.compress();
+        println!("{:?}", bcdc);
+        let bcdcd = bcdc.decompress().unwrap();
+        println!("{:?}", bcdcd);
 
-    }
-
-    #[ignore]
-    #[test]
-    fn get_ristretto_basepoint_multiples() {
-        let mut P = EdwardsPoint::identity();
-
-
-        /*for i in 0..15 {
-            println!("{:?}", hex::encode(data: T))
-        }*/
+        assert!(bcdcd == constants::RISTRETTO_BASEPOINT);
     }
 }
 
