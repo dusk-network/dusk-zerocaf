284c3943d985b65f98248c3120937bd23ce6afdb
kr0 <c.perezbaro@gmail.com>
2019-09-14 00:07:35 +0200

    Implement encoding/decoding and test Basepoint validity.
    The compression and decompression formulas have been
    tested for the unique basepoint taken as a
    `RistrettoBasepoint`.
    
    All checks passed, compression and decompression worked
    and the resulting point verified the equalty testing.

--- src/ristretto.rs
+++ src/ristretto.rs
@@ -31,7 +31,7 @@ use core::ops::{Add, Mul, Neg, Index};
 
 use std::fmt::Debug;
 
-use subtle::{Choice, ConstantTimeEq};
+use subtle::{Choice, ConstantTimeEq, ConditionallyNegatable};
 
 
 /// Ristretto Point expressed in wire format.
@@ -76,7 +76,63 @@ impl CompressedRistretto {
     /// - If the decompression/decoding succeeds -> `Some(RistrettoPoint)`. 
     /// - If the decompression/decoding fails -> `None`.
     pub fn decompress(&self) -> Option<RistrettoPoint> {
-        unimplemented!()
+        // Step 1: Check that the byte-string is a valid FieldElement. 
+
+        // As Ristretto paper says: "If the implementation's field element
+        // encoding function produces canonical outputs, one way to check 
+        // that s_bytes is a canonical encoding (in step 1) is to decode 
+        // s_bytes into sss, then re-encode sss into s_bytes_check, and ensure 
+        // that s_bytes == s_bytes_check.
+        let s: FieldElement = FieldElement::from_bytes(&self.as_bytes());
+        let s_check = s.to_bytes();
+        let s_correct_enc = s_check.ct_eq(&self.as_bytes());
+        let s_is_positive = s.is_positive();
+
+        // If the byte-encoding was incorrect or the representation is
+        // a negative `FieldElement` (according to the definition of 
+        // positive found on Decaf paper), return `None`. 
+        println!("Is positive? {:?} Is correct encoded?{:?}", s_is_positive.unwrap_u8(), s_correct_enc.unwrap_u8());
+        if s_is_positive.unwrap_u8() == 0u8 || s_correct_enc.unwrap_u8() == 0u8 {
+            return None
+        };
+        // Step 2: Attempt to decompress the CompressedRistretto. 
+        let one = FieldElement::one();
+
+        // u1 = 1 + as² with a = -1. 
+        let u1 = one - s.square();
+        // u2 = 1 - as² with a = -1. 
+        let u2 = one + s.square();
+        let u2_sq = u2.square();
+
+        // v = a*d*u1² - u2²
+        let v = -(constants::EDWARDS_D * u1.square()) - u2_sq;
+        // I = 1/sqrt(v*u2²), returns `None` if the sqrt does not exist. 
+        let (ok, I) = (v*u2_sq).inv_sqrt();
+        if ok.unwrap_u8() == 0 {return None};
+        
+        // Compute the Extended Point Coordinates Y & T
+        let Dx = I*u2;
+        let Dy = I*Dx*v;
+
+        // Compute ABS(2*s*Dx) and negate if it is negative.
+        let mut x = (&s + &s) * Dx;
+        let x_is_pos = x.is_positive();
+        x.conditional_negate(!x_is_pos);
+        // Compute Y and T coordinates. 
+        let y = u1 * Dy;
+        let t = x * y;
+
+        println!("Is QR? {:?}  Is positive? {:?} Y is zero?  {:?}",ok.unwrap_u8(), t.is_positive().unwrap_u8(), y);
+        if t.is_positive().unwrap_u8() == 0u8 || y == FieldElement::zero() {
+            return None
+        };
+
+        Some(RistrettoPoint(EdwardsPoint {
+            X: x,
+            Y: y,
+            Z: one,
+            T: t
+        }))
     }
 }
 
@@ -95,7 +151,8 @@ impl ConstantTimeEq for RistrettoPoint {
     /// and we are on the twisted case, we compare 
     /// X1*Y2 == Y1*X2. 
     fn ct_eq(&self, other: &RistrettoPoint) -> Choice {
-        unimplemented!()
+        (self.0.X * other.0.Y).to_bytes().ct_eq(
+            &(self.0.Y * other.0.X).to_bytes())
     }
 }
 
@@ -233,8 +290,33 @@ impl Mul<Scalar> for RistrettoPoint {
 impl RistrettoPoint {
     /// Encode a Ristretto point represented by the point `(X:Y:Z:T)`
     /// in extended coordinates. 
+    #[allow(non_snake_case)]
     pub fn compress(&self) -> CompressedRistretto {
-        unimplemented!()
+        let u1 = (self.0.Z + self.0.Y) * (self.0.Z - self.0.Y);
+        let u2 = self.0.X * self.0.Y;
+        let (_, I) = (u1 * u2.square()).inv_sqrt();
+        let D1 = u1 * I;
+        let D2 = u2 * I;
+        let Zinv = D1 * D2 * self.0.T;
+        let mut xy;
+        let D;
+        if (self.0.T * Zinv).is_positive().unwrap_u8() == 0u8 {
+            xy = 
+                (constants::SQRT_MINUS_ONE * self.0.Y,
+                constants::SQRT_MINUS_ONE * self.0.X);
+            D = D1 * constants::INV_SQRT_A_MINUS_D;
+        } else {
+            xy = (self.0.X, self.0.Y);
+            D = D2;
+        };
+
+        xy.1.conditional_negate(!(xy.0*Zinv).is_positive());
+        // We are on the Twisted case, so a = -1. 
+        // Then s = ABS((Z-Y) * D)
+        let mut s = (self.0.Z - xy.1) * D;
+        s.conditional_negate(!s.is_positive());
+
+        CompressedRistretto(s.to_bytes()) 
     }
 
     /// Computes the Ristretto Elligator map.
@@ -251,5 +333,13 @@ impl RistrettoPoint {
 mod tests {
     use hex;
     use super::*;
+
+    #[test]
+    fn basepoint_compr_decompr() {
+        let compress = RistrettoPoint(constants::BASEPOINT).compress();
+
+        let decompress = compress.decompress().unwrap();
+        assert!(decompress == RistrettoPoint(constants::BASEPOINT));
+    }
 }
 
