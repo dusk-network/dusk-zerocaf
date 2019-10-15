4dfe10ddec2e2f6716d6a63ec1ea257d1e41d5d1
kr0 <c.perezbaro@gmail.com>
2019-10-15 12:17:12 +0200

    Update benchmarks to Criterion v0.3
    Implemented benchmarks for `FieldElement` ops and
    blackboxed the inputs.

--- benchmarks/dusk_benchmarks.rs
+++ benchmarks/dusk_benchmarks.rs
@@ -5,12 +5,12 @@ extern crate criterion;
 
 use criterion::{Benchmark, criterion_group, criterion_main, Criterion, BenchmarkId};
 
-use zerocaf::backend::u64::{field, scalar};
 use zerocaf::edwards::*;
-use zerocaf::ristretto::RistrettoPoint;
+use zerocaf::ristretto::*;
 use zerocaf::traits::ops::*;
 use zerocaf::constants::*;
 use zerocaf::field::*;
+use zerocaf::scalar::*;
 #[allow(unused_imports)]
 use zerocaf::traits::Identity;
 
@@ -18,11 +18,9 @@ mod field_benches {
 
     use super::*;
     use subtle::Choice;
-    use zerocaf::field::FieldElement;
-    use zerocaf::scalar::Scalar;
 
     /// `B = 904625697166532776746648320197686575422163851717637391703244652875051672039`
-    pub static B: field::FieldElement = field::FieldElement([
+    pub static B: FieldElement = FieldElement([
         2766226127823335,
         4237835465749098,
         4503599626623787,
@@ -39,31 +37,37 @@ mod field_benches {
         let pow = 249u64;
 
         c.bench_with_input(
-            BenchmarkId::new("Addition", "Fixed constants"), &inp , |b, &inp| {
+            BenchmarkId::new("Addition", "Fixed FieldElements"), &inp , |b, &inp| {
                 b.iter(|| inp.1 + inp.0);
             }
         );
 
         c.bench_with_input(
-            BenchmarkId::new("Subtraction", "Fixed constants"), &inp , |b, &inp| {
+            BenchmarkId::new("Subtraction", "Fixed FieldElements"), &inp , |b, &inp| {
                 b.iter(|| inp.0 - inp.1);
             }
         );
 
         c.bench_with_input(
-            BenchmarkId::new("Multiplication", "Fixed constants"), &inp , |b, &inp| {
+            BenchmarkId::new("Multiplication", "Fixed FieldElements"), &inp , |b, &inp| {
                 b.iter(|| inp.0 * inp.1);
             }
         );
 
         c.bench_with_input(
-            BenchmarkId::new("Square", "Fixed constants"), &inp , |b, &inp| {
+            BenchmarkId::new("Division", "Fixed FieldElements"), &inp , |b, &inp| {
+                b.iter(|| inp.0/inp.1);
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Square", "Fixed FieldElements"), &inp , |b, &inp| {
                 b.iter(|| inp.1.square());
             }
         );
 
         c.bench_with_input(
-            BenchmarkId::new("Half", "Fixed constants"), &inp , |b, &inp| {
+            BenchmarkId::new("Half", "Fixed FieldElements"), &inp , |b, &inp| {
                 b.iter(|| inp.0.half());
             }
         );
@@ -75,19 +79,114 @@ mod field_benches {
         );
 
         c.bench_with_input(
-            BenchmarkId::new("Power", "Fixed constants"), &inp , |b, &inp| {
+            BenchmarkId::new("Power", "Fixed FieldElements"), &inp , |b, &inp| {
                 b.iter(|| inp.0.pow(&inp.1));
             }
         );
 
         c.bench_with_input(
-            BenchmarkId::new("Modular Sqrt", "Fixed constants"), &inp2 , |b, &inp| {
+            BenchmarkId::new("Legendre Symbol", "Fixed FieldElement"), &inp , |b, &inp| {
+                b.iter(|| inp.0.legendre_symbol());
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Modular Sqrt", "Fixed FieldElements"), &inp2 , |b, &inp| {
                 b.iter(|| inp2.0.mod_sqrt(inp2.1));
             }
         );
+
         c.bench_with_input(
-            BenchmarkId::new("Modular inverse", "Fixed constants"), &inp , |b, &inp| {
+            BenchmarkId::new("Modular inverse", "Fixed FieldElements"), &inp , |b, &inp| {
                 b.iter(|| inp.0.inverse());
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Sqrt ratio i", "Fixed FieldElements"), &inp , |b, &inp| {
+                b.iter(|| inp.0.sqrt_ratio_i(&inp.1));
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Inverse sqrt", "Fixed FieldElements"), &inp , |b, &inp| {
+                b.iter(|| inp.0.inv_sqrt());
+            }
+        );
+    }
+}
+
+mod scalar_benches {
+    use super::*;
+    use zerocaf::scalar::Scalar;
+
+    /// `C = 182687704666362864775460604089535377456991567872`.
+    pub static C: Scalar = Scalar([0, 0, 0, 2, 0]);
+
+    /// `D = 904625697166532776746648320197686575422163851717637391703244652875051672039`
+    pub static D: Scalar = Scalar([
+        2766226127823335,
+        4237835465749098,
+        4503599626623787,
+        4503599627370493,
+        2199023255551,
+    ]);
+
+    pub fn bench_scalar_element_ops(c: &mut Criterion) {
+        let inp = (C, D);
+        let pow = 215u64;
+
+        c.bench_with_input(
+            BenchmarkId::new("Addition", "Fixed Scalars"), &inp , |b, &inp| {
+                b.iter(|| inp.0 + inp.1);
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Subtraction", "Fixed Scalars"), &inp , |b, &inp| {
+                b.iter(|| inp.0 - inp.1);
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Mul", "Fixed Scalars"), &inp , |b, &inp| {
+                b.iter(|| inp.0 * inp.1);
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Squaring", "Fixed Scalars"), &inp , |b, &inp| {
+                b.iter(|| inp.1.square());
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Half", "Fixed Scalars"), &inp , |b, &inp| {
+                b.iter(|| inp.0.half());
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Two pow K", "Fixed Scalar"), &pow , |b, &pow| {
+                b.iter(|| Scalar::two_pow_k(pow));
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Power", "Fixed Scalars"), &inp , |b, &inp| {
+                b.iter(|| inp.0.pow(&inp.1));
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Into-bits conversion", "Fixed Scalar"), &inp , |b, &inp| {
+                b.iter(|| inp.1.into_bits());
+            }
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("Shr", "Fixed Scalar"), &inp , |b, &inp| {
+                b.iter(|| inp.1 >> 135);
             }
         );
     }
@@ -96,6 +195,7 @@ mod field_benches {
 criterion_group!(
     benchmarks,
     field_benches::bench_field_element_ops,
+    scalar_benches::bench_scalar_element_ops,
 );
 criterion_main!(
     benchmarks,
